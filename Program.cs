55077a3e28c384bab8097d6d9cfb79f5bb651f38
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-27 14:34:36 +0530

    [Rupali]ADDED: Ability to delet first element in linked list

--- Program.cs
+++ Program.cs
@@ -6,18 +6,27 @@ namespace LinkListProblems
     {
         static void Main(string[] args)
         {
-
-            {
-                LinkListOperation listOperation = new LinkListOperation();
-                listOperation.insert(56);
-                listOperation.insert(30);
-                listOperation.insert(70);
-                listOperation.display();
-
-
-                listOperation.insertAfter(30, 56);
-                listOperation.display();
-            }
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+            //Ablity To Create Linked List By Appending
+            LinkListOperation linkedList = new LinkListOperation();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList = new LinkListOperation();
+            linkedList.Add2(70);
+            linkedList.Add2(30);
+            linkedList.Add2(56);
+            linkedList.Display();
+            linkedList = new LinkListOperation();
+            linkedList.Add(56);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("\n");
+            linkedList.InsertAtParticularPoistion(2, 30);
+            linkedList.Display();
+            Console.WriteLine("First element is poped out from linked list = " + linkedList.removeFirstNode().data);
+            linkedList.Display();
         }
     }
-
+}
